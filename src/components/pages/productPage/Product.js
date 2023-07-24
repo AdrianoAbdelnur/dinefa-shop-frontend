e8b18cd755eb99752f607755f9b42531de4f4358
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2023-07-24 09:18:19 -0300

    Se agrega pagina de detalles de producto

---
+++ src/components/pages/productPage/Product.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './product.css'
+
+const Product = () => {
+  return (
+    <div className='product_container'>
+        
+    </div>
+  )
+}
+
+export default Product
