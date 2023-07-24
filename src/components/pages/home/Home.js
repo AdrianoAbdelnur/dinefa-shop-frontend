5b92b808fd671828271664de5c7e3fe8bd193752
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2023-07-24 09:16:41 -0300

    Se obtienen productos de API

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,24 +1,82 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './home.css'
 import ProductCard from '../../productCard/ProductCard'
 import { CarouselComponent } from '../../carousel/CarouselComponent'
+import axios from 'axios'
 
 
 const Home = () => {
+  const [products, setproducts] = useState([])
+
+
+useEffect(() => {
+  getProducts()
+}, [])
+
+const getProducts = async() => {
+  try {
+    const {data}= await axios("http://localhost:4000/api/product/getAllProducts")
+    setproducts(data.products)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
   return (
     <div className='home_container'>
       <div className='w-100 m-3'>
         <CarouselComponent/>
       </div>
       <div className='products'>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
+        {
+          products.length?
+            products.map((product)=>{
+              return (
+                <ProductCard
+                  product={product}
+                />
+              )
+            })
+            :
+            "No hay productos para mostrar"
+        }
+        {
+          products.length?
+            products.map((product)=>{
+              return (
+                <ProductCard
+                  product={product}
+                />
+              )
+            })
+            :
+            "No hay productos para mostrar"
+        }
+        {
+          products.length?
+            products.map((product)=>{
+              return (
+                <ProductCard
+                  product={product}
+                />
+              )
+            })
+            :
+            "No hay productos para mostrar"
+        }
+        {
+          products.length?
+            products.map((product)=>{
+              return (
+                <ProductCard
+                  product={product}
+                />
+              )
+            })
+            :
+            "No hay productos para mostrar"
+        }
       </div>
 
     </div>
