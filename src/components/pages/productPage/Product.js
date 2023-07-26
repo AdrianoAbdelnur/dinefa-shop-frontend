ed88b95845b40f8ee51a34da75519c8bad5a5b97
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2023-07-26 07:47:53 -0300

    Pagina de productos agregada

--- src/components/pages/productPage/Product.js
+++ src/components/pages/productPage/Product.js
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
 import './product.css'
+import { useLocation } from 'react-router-dom'
+import axios from 'axios';
+import { Image } from 'react-bootstrap';
 
 const Product = () => {
+  const location = useLocation();
+  const [productId, setProductId] = useState("")
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    setProductId(location.hash.substring(1))
+  }, [])
+  
+  useEffect(() => {
+    if(productId) {
+      getProduct(productId);
+    }
+  }, [productId])
+
+  const getProduct = async(id) => {
+    try {
+      const {data} = await axios('http://localhost:4000/api/product/getProduct/'+ id)
+      console.log(data.product)
+      setProduct(data.product)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+
+
   return (
     <div className='product_container'>
-        
+      <div className='productImage_container'>
+        <Image src={product.image} rounded className='w-100'/>
+      </div>
+      <div className='price_contaier'>
+        <h4>{product.brand}</h4>
+          <h5>{product.name}</h5>
+        <p><b>${product.price}</b></p>
+      </div>
     </div>
   )
 }
--- src/components/productCard/ProductCard.js
+++ src/components/productCard/ProductCard.js
@@ -3,10 +3,11 @@ import './productCard.css'
 import { Button, Card} from 'react-bootstrap'
 import televisor from './../../assets/img/istockphoto-638043774-612x612.jpg'
 
-const ProductCard = ({product}) => {
+const ProductCard = ({product, setShowToProduct}) => {
+
   return (
-    <div className='productCard_container'>
-        <Card className='productCard'>
+    <div className='productCard_container' onClick={()=>setShowToProduct(product)}>
+        <Card className='productCard' >
         <Card.Img variant="top" src={televisor} />
             <Card.Body>
                 <Card.Title className='productCard_title'> {product?.name} - Marca: {product?.brand}</Card.Title>
