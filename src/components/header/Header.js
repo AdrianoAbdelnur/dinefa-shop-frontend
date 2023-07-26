6feb7725cf2307a9ad2fb14b9e090620d8d9c078
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2023-07-26 07:55:31 -0300

    Se agrega cart

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './header.css'
 import logo from './../../assets/img/logo-dinefa.png'
-import {Form, InputGroup } from 'react-bootstrap'
+import {Button, Form, InputGroup } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
+  let navigate = useNavigate();
   return (
     <div className='header_container'>
       <img src={logo} alt='logo Dinefa' className='logo'></img>
@@ -15,12 +17,17 @@ const Header = () => {
           aria-describedby="search"
         />
       </InputGroup>
+      <div>
+        <div>
+          <Button onClick={()=>navigate('/carts')}>Cart</Button>
+        </div>
         <div className='navbar_options'>
             <a href='login' id='login'>INGRESAR</a>
             <a href='facebook.com' id='register'>REGISTRARSE</a>
             <a href='facebook.com' id='register'>AYUDA</a>
             <a href='facebook.com' id='register'>CONTACTO</a>
         </div>
+      </div>
     </div>
   )
 }
