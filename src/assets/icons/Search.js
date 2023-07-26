4ac9140c0864de1743b659082b27d9f310c78b54
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2023-07-26 09:07:31 -0300

    Se agrega register

---
+++ src/assets/icons/Search.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="28" height="28" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+        <path d="M21 21l-6 -6" />
+    </svg>
+  )
+}
+
+export default Search
---
+++ src/assets/icons/X.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const X = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-x" width="30" height="30" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ff2825" fill="none" strokeLinecap="round" strokeLinejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <line x1="18" y1="6" x2="6" y2="18" />
+  <line x1="6" y1="6" x2="18" y2="18" />
+</svg>
+  )
+}
+
+export default X
