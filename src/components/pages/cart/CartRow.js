0532e2cdd62f63ce0eae3658e22b41b27a201bdf
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2023-08-10 14:07:19 -0300

    fix in buttons

--- src/components/pages/cart/CartRow.js
+++ src/components/pages/cart/CartRow.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { Button } from 'react-bootstrap'
+
 
 const CartRow = ({product}) => {
     const [quantityAdd, setQuantityAdd] = useState(0)
@@ -7,9 +9,9 @@ const CartRow = ({product}) => {
         <td>1</td>  
         <td>{product.idProduct.name}</td>
         <td className='d-flex justify-content-around '>
-        <button onClick={()=>setQuantityAdd(quantityAdd- 1)}>-</button>
+        <Button onClick={()=>setQuantityAdd(quantityAdd- 1)}>-</Button>
         {product.quantity+quantityAdd}
-        <button onClick={()=>setQuantityAdd(quantityAdd+ 1)}>+</button>
+        <Button onClick={()=>setQuantityAdd(quantityAdd+ 1)}>+</Button>
         </td>
         <td>{product.price}</td>
         <td>{product.price*(product.quantity+quantityAdd)}</td>
