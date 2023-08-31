54568a7ec081378ef581e0188f0dc50848adad63
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2023-08-31 14:49:23 -0300

    Upload image function added

---
+++ src/components/pages/admin/products/FileButton.js
@@ -0,0 +1,52 @@
+import { useRef } from "react";
+import { Button } from "react-bootstrap"
+
+export default function FileButton({setFile}) {
+    const hiddenFileInput = useRef(null);
+    const handleClick = (e) => {
+        hiddenFileInput.current.click()
+    }
+
+    const handleChange = async(e) => {
+        const fileUploaded = e.target.files[0]
+        beforeUpload(fileUploaded);
+        const base64 = await getBase64(fileUploaded);
+        console.log(base64)
+        setFile(base64)
+    }
+
+    const getBase64 = (img) => {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.addEventListener("load", () => resolve(reader.result));
+            reader.readAsDataURL(img)
+        })
+    }
+
+    const beforeUpload = (file) => {
+        const isJpegOrPng = file.type === "image/jpeg" || file.type === "image/png"
+        if (!isJpegOrPng) {
+            alert("Solamente se permiten archivos jpeg o png")
+            return;
+        }
+    }
+
+    const onInputClick = (e)=> {
+        e.target.value = "";
+    }
+
+    return (
+        <>
+            <Button variant="dark" onClick={handleClick}>
+                Seleccionar Imagen del Producto
+            </Button>
+            <input 
+            type="file"
+            ref={hiddenFileInput}
+            style={{display: "none"}}
+            onChange={handleChange}
+            onClick={onInputClick}
+            ></input>
+        </>
+    )
+}
--- src/components/pages/admin/products/modal/ProductModal.js
+++ src/components/pages/admin/products/modal/ProductModal.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 import { Button, Col, Form, Modal, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
+import FileButton from '../FileButton';
 
 const ProductModal = ({ show, setShow }) => {
     const [showInputDetails, setShowInputDetails] = useState(false)
+    const [image, setImage] = useState()
+    
 
     const handleClose = () => setShow(false);
     return (
@@ -11,81 +14,79 @@ const ProductModal = ({ show, setShow }) => {
             <Modal.Header closeButton>
                 <Modal.Title>Modal heading</Modal.Title>
             </Modal.Header>
-            <Modal.Body>
-                <Form>
-                    <Form.Group className="mb-3" controlId="name">
-                        <Form.Label>Nombre</Form.Label>
-                        <Form.Control type="text" placeholder="Nombre del producto" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="brand">
-                        <Form.Label>Marca</Form.Label>
-                        <Form.Control type="text" placeholder="Marca del producto" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="model">
-                        <Form.Label>Model</Form.Label>
-                        <Form.Control type="text" placeholder="Modelo del producto" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="model">
-                        <Form.Select aria-label="Floating label select example">
-                            <option>Seleccione una Categoría</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </Form.Select>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="description">
-                        <Form.Label>Descripción:</Form.Label>
-                        <Form.Control as="textarea" placeholder="Agregue la descripción del producto" />
-                    </Form.Group>
-                    {
-                        !showInputDetails &&
-                        <Link 
-                            className="addDetail"
-                            onClick={()=>setShowInputDetails(true)}
-                        >
-                            Añadir detalle
-                        </Link>
-                    }
-                    {
-                        showInputDetails && <div>
-                            <Form.Group className="mb-3" controlId="details">
-                                <Form.Label>Detalles:</Form.Label>
-                                <Row>
-                                    <Col>
-                                        <Form.Control type="text" placeholder=" Característica" />
-                                    </Col>
-                                    <Col>
-                                        <Form.Control type="text" placeholder="Detalle" />
-                                    </Col>
-                                </Row>
-                            </Form.Group>
-                            <Link
-                                className=""
-                                onClick={()=>setShowInputDetails(false)}
-                                >
-                                Añadir
+            <Form>
+                <Modal.Body>
+                        <Form.Group className="mb-3" controlId="name">
+                            <Form.Label>Nombre</Form.Label>
+                            <Form.Control type="text" placeholder="Nombre del producto" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="brand">
+                            <Form.Label>Marca</Form.Label>
+                            <Form.Control type="text" placeholder="Marca del producto" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="model">
+                            <Form.Label>Model</Form.Label>
+                            <Form.Control type="text" placeholder="Modelo del producto" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="model">
+                            <Form.Select aria-label="Floating label select example">
+                                <option>Seleccione una Categoría</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </Form.Select>
+                        </Form.Group>
+                        <FileButton
+                            setFile={setImage}
+                        />
+                        <Form.Group className="mb-3" controlId="description">
+                            <Form.Label>Descripción:</Form.Label>
+                            <Form.Control as="textarea" placeholder="Agregue la descripción del producto" />
+                        </Form.Group>
+                        {
+                            !showInputDetails &&
+                            <Link 
+                                className="addDetail"
+                                onClick={()=>setShowInputDetails(true)}
+                            >
+                                Añadir detalle
                             </Link>
-                        </div>
-                    }
-
-
-                    <Form.Group className="mb-3" controlId="price">
-                        <Form.Label>Precio</Form.Label>
-                        <Form.Control type="number" placeholder="Ingrese el Precio de lista" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
+                        }
+                        {
+                            showInputDetails && <div>
+                                <Form.Group className="mb-3" controlId="details">
+                                    <Form.Label>Detalles:</Form.Label>
+                                    <Row>
+                                        <Col>
+                                            <Form.Control type="text" placeholder=" Característica" />
+                                        </Col>
+                                        <Col>
+                                            <Form.Control type="text" placeholder="Detalle" />
+                                        </Col>
+                                    </Row>
+                                </Form.Group>
+                                <Link
+                                    className=""
+                                    onClick={()=>setShowInputDetails(false)}
+                                    >
+                                    Añadir
+                                </Link>
+                            </div>
+                        }
+                        <Form.Group className="mb-3" controlId="price">
+                            <Form.Label>Precio</Form.Label>
+                            <Form.Control type="number" placeholder="Ingrese el Precio de lista" />
+                        </Form.Group>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
                     </Button>
-                </Form>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                    Close
-                </Button>
-                <Button variant="primary" onClick={handleClose}>
-                    Save Changes
-                </Button>
-            </Modal.Footer>
+                    <Button variant="primary" type="submit" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Form>
         </Modal>
     )
 }
